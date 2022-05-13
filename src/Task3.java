0783b09fd64a049ef8f0f6a94e0e11e28935a4a7
Santiago <santisaavcon@gmail.com>
2022-05-12 23:18:28 -0400

    Task3 code added

--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,70 @@
+import java.util.Scanner;
+
 public class Task3 {
+
+    public static void main (String [] args)
+
+    {
+
+        Scanner scaner = new Scanner(System.in);
+
+        scaner.useDelimiter("\\n");
+
+        int count=0; int hiddernumber; while (count<3)
+
+    {
+
+        hiddernumber = (int) ((Math.random() * 100) + 1); System.out.println("*******************************************************");
+
+        System.out.println("********Guess the number random between 1 - 100********");
+
+        System.out.println("*******************************************************");
+
+        System.out.println("Number hidden is " + hiddernumber); int win=0;
+
+        while (win < 1) {
+
+            System.out.println("***************************************");
+
+            System.out.println("Enter a Number");
+
+            int number = scaner.nextInt(); if (number != hiddernumber) {
+
+                if (number < hiddernumber) {
+
+                    System.out.println("***************************************");
+
+                    System.out.println("Enter a Major Number to " + number);
+
+                } else {
+
+                    System.out.println("***************************************");
+
+                    System.out.println("Enter a Less Number to " + number);
+
+                }
+
+            }
+
+            if (number == hiddernumber) {
+
+                System.out.println("***************************************");
+
+                System.out.println("Congratulations! the Number hidden was " + hiddernumber);
+
+                win++;
+
+            }
+
+        }count++;
+
+    }
+
+    }
+
 }
+
+//add feature "number of attempts"
+
+//keep the play 3 times feature
+
