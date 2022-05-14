4eeddbe50c8adf98e730a41f15c98c3b39a20dea
Santiago <santisaavcon@gmail.com>
2022-05-14 11:46:46 -0400

    Added welcome messages

--- src/Printer.java
+++ src/Printer.java
@@ -1,2 +1,18 @@
 public class Printer {
+
+
+    // This method shows the title at the start of the game.
+    private void showTitle(){
+        int CHARACTERS_HEIGHT = 50; //To have a uniform size in the console.
+        System.out.println("*".repeat(CHARACTERS_HEIGHT));
+        System.out.println("******** Guess the number between 1 - 100 ********");
+        System.out.println("*".repeat(CHARACTERS_HEIGHT));
+    }
+
+    // This method asks the user to show a title.
+    private void askForGuess(){
+        int CHARACTERS_HEIGHT = 50;
+        System.out.println("*".repeat(CHARACTERS_HEIGHT));
+        System.out.print("Enter a Number: "); // The input will be: Enter a number: 8
+    }
 }
