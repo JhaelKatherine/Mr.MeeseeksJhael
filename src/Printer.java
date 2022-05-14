b233407716e7371341dffcf8c4ebce29329ce4a5
Santiago <santisaavcon@gmail.com>
2022-05-14 15:46:53 -0400

    Added builder + final atributte

--- src/Printer.java
+++ src/Printer.java
@@ -1,9 +1,13 @@
 public class Printer {
+    private final int CHARACTERS_HEIGHT; //To have a uniform size in the console.
+
+    Printer(){
+        CHARACTERS_HEIGHT = 50;
+    }
 
 
     // This method shows the title at the start of the game.
     private void showTitle(){
-        int CHARACTERS_HEIGHT = 50; //To have a uniform size in the console.
         System.out.println("*".repeat(CHARACTERS_HEIGHT));
         System.out.println("******** Guess the number between 1 - 100 ********");
         System.out.println("*".repeat(CHARACTERS_HEIGHT));
@@ -11,7 +15,6 @@ public class Printer {
 
     // This method asks the user to show a title.
     private void askForGuess(){
-        int CHARACTERS_HEIGHT = 50;
         System.out.println("*".repeat(CHARACTERS_HEIGHT));
         System.out.print("Enter a Number: "); // The input will be: Enter a number: 8
     }
