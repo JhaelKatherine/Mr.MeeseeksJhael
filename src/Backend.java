454f98692bc479ab4259c47f7e8b295864f3c6c6
JhaelKatherine <101666151+JhaelKatherine@users.noreply.github.com>
2022-05-14 12:40:46 -0400

    Update Backend.java

--- src/Backend.java
+++ src/Backend.java
@@ -1,2 +1,22 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Backend {
+    public void inputNumber(){
+        Scanner scanner = new Scanner(System.in);
+        boolean execute;
+        do {
+            try {
+                execute = false;
+                //message "Enter a Number"
+                int number=  scanner.nextInt();
+            }
+            catch (InputMismatchException ex) {
+                //message "You must enter an integer"
+                scanner.next();
+                execute = true;
+            }
+        } while (execute);
+
+    }
 }
